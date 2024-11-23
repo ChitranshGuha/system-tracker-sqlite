195ebbf1eb5255e307976a9f99885805c13f79eb
hemantreact500 <hemantreact500@gmail.com>
2024-11-23 18:40:33 +0530

    Fix

--- components/PastActivities.js
+++ components/PastActivities.js
@@ -184,10 +184,10 @@ export default function PastActivities({authToken,ownerId}) {
                       : "-"
                     }
                   </td>
-                  <td className="px-4 py-3 whitespace-nowrap text-center">{Number(activity.totalTime / 60).toFixed(2)}</td>
+                  <td className="px-4 py-3 whitespace-nowrap text-center">{Number(activity.totalTime / 60).toFixed(1)}</td>
                   <td className={`px-4 py-3 whitespace-nowrap text-center ${TEXT_COLORS[0]}`}>{activity.mouseClick}</td>
                   <td className={`px-4 py-3 whitespace-nowrap text-center ${TEXT_COLORS[1]}`}>{activity.keystroke}</td>
-                  <td className={`px-4 py-3 whitespace-nowrap text-center ${TEXT_COLORS[2]}`}>{activity.idleTime}</td>
+                  <td className={`px-4 py-3 whitespace-nowrap text-center ${TEXT_COLORS[2]}`}>{Number(activity.idleTime / 60).toFixed(2)}</td>
                 </tr>
               ))}
             </tbody>
