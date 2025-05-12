090eaf3877d308a82f570fcd22b24dd8eea92339
hemantreact500 <hemantreact500@gmail.com>
2025-05-12 13:48:35 +0530

    Version update

--- utils/constants.js
+++ utils/constants.js
@@ -6,7 +6,7 @@ export const BASE_URL = `https://webtracker${IS_PRODUCTION ? 'prod' : ''}.infowa
 
 export const API_BASE_URL = `${BASE_URL}/api`;
 
-export const TRACKER_VERSION = '1.0.1';
+export const TRACKER_VERSION = '1.0.2';
 
 export const APP_DOWNLOAD_URL = IS_PRODUCTION
   ? `https://infowarewebtrackerprod.s3.ap-south-1.amazonaws.com/a2ee38f6-507a-41ae-bbb4-72d2e17b29ed/System%20Tracker%20Setup%20${TRACKER_VERSION}%20${DOMAIN_TYPE}.exe`
